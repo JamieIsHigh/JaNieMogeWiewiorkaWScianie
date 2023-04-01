93532b5f8f46bdc77911f439bbfb94578d97eca4
JamieIsHigh <113109707+JamieIsHigh@users.noreply.github.com>
2023-04-02 01:19:46 +0200

    Deklaracja w kodzie

--- app/src/main/java/com/example/janiemogewiewiorkawscianie/MainActivity.kt
+++ app/src/main/java/com/example/janiemogewiewiorkawscianie/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.janiemogewiewiorkawscianie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ileraz = findViewById<EditText>(R.id.editTextNumber).text.toString().toInt()
+        val iledan = findViewById<EditText>(R.id.editTextNumber2).text.toString().toInt()
+
+        val textBru = findViewById<TextView>(R.id.textView4).text
+        val textKMP = findViewById<TextView>(R.id.textView5).text
+        val textBM = findViewById<TextView>(R.id.textView6).text
+        val textRK = findViewById<TextView>(R.id.textView7).text
     }
 }
